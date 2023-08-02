29804398f385d2d12257f04cb5f6ed1a0e4744a6
2355033t <2355033t@stu.kobe-u.ac.jp>
2023-08-02 23:13:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import pprint
+import numpy as np
 
 PAI_N = 44
 PAI_KIND = 20
@@ -12,9 +13,10 @@ class Game:
     def __init__(self):
         self.yama = [3] * 9 + [1] * 9 + [4] * 2
         self.hands = [[0] * PAI_KIND for _ in range(PLAYER_N)]
-        self.rivers = [[] for _ in range(PLAYER_N)]
+        self.rivers = [[-1] * RIVER_N for _ in range(PLAYER_N)]
         self.deal()
         self.turn = -1
+        self.round = 0
         self.next()
 
     def emit(self):
@@ -32,14 +34,22 @@ class Game:
                 self.hands[i][self.emit()] += 1
 
     def next(self):
-        self.turn = (self.turn + 1) % PLAYER_N
+        self.turn += 1
+        if self.turn == 4:
+            self.turn = 0
+            self.round += 1
         self.hands[self.turn][self.emit()] += 1
 
     def play(self, i):
         self.hands[self.turn][i] -= 1
-        self.rivers[self.turn].append(i)
+        self.rivers[self.turn][self.round] = i
         self.next()
+
+class Gene:
+    def __init__(self):
+        self.w = np.random.rand(PAI_KIND + RIVER_N * PLAYER_N + 1, PAI_KIND)
 
 if __name__ == "__main__":
     game = Game()
-    pprint.pprint(game.hands)
+    x = game.hands[0] + sum(game.rivers, [])
+    print(x)
